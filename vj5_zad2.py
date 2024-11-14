c7b4e6b85c87b428de3afec58a47a6ca5d47f2e0
MarkoLevi17 <markolevi59@gmail.com>
2024-11-14 09:35:44 +0100

    Update vj5_zad2.py

--- vj5_zad2.py
+++ vj5_zad2.py
@@ -1,13 +1,1 @@
-def provjera_broja (broj): 
-    if 10 <= broj <= 100:
-        return f"Broj {broj} je unutar raspona."
-    else:
-    
-        return f"Broj {broj} je izvan raspona."
-if __name__ == "__main__":
-    try:
-    print (provjera broja (prvi broj))
-    drugi_broj = int(input("Unesite drugi broj: "))
-    print (provjera_broja (drugi_broj))
-    except ValueError:
-    print("Unesena vrijednost nije broj.")
+
